68231ee8fa5aff5c54c7dd454d4746f4ca13546d
MananKukreja24 <96042112+MananKukreja24@users.noreply.github.com>
2023-08-06 20:08:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Manan Kukreja</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project.php">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <li class="nav-item"> 
+    <a class="nav-link" href="https://drive.google.com/file/d/1ZIRVzrVdvrjFCEc0RwUhY9m9epqjperT/view?usp=drive_link">Resume</a>
+  </li>
+      
+        
+      
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/cc1.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+       
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/cc2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+       
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/cc3.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+     
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<section class="my-5">
+    <div class="py-5"> 
+        <h2 class="text-center">
+            About Me
+        </h3>
+    </div>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/profile1.jpg"  class="img-fluid aboutimg" >
+        </div>
+        <div class="col-lg-6 col-md-6 col-12">
+           <h2 class="display-4">I am Manan Kukreja.</h2>
+           <p class="py-3">
+I am a dedicated final year Computer Science student at MIT-ADT University, Pune. Proficient in HTML, CSS, JavaScript, Node.js, Express.js, MongoDB, MySQL, PHP, C, and C++, I possess a robust skill set spanning web development, backend programming, and database management. Throughout my academic journey, I have cultivated a passion for problem-solving, an analytical mindset, and effective communication skills. As I approach the culmination of my studies, I am enthusiastic about utilizing my diverse expertise to contribute meaningfully to the tech industry and beyond.</p>
+<a href="about.php" class="button">check more</a>
+        </div>
+
+    </div>
+</div>
+</section>
+<section class="my-5">
+    <div class="py-5"> 
+        <h2 class="text-center">
+            Projects
+        </h2>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/sec1.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">ARK</h4>
+    <p class="card-text">I have created a website using HTML5 and CSS3.</p>
+    <a href="https://github.com/MananKukreja24/phishing-prevention-url" class="btn btn-primary">See Project</a>
+  </div>
+</div>
+
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/port.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Portfolio</h4>
+    <p class="card-text">I have created a fully responsive portfolio website using HTML5,CSS3,PHP,MySQL.</p>
+    <a href="https://github.com/MananKukreja24/Portfolio" class="btn btn-primary">See Project</a>
+  </div>
+</div>
+
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/port1.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">MIT-Gram</h4>
+    <p class="card-text">I have created a website for my college events using HTML,CSS,JavaScript,Node.js,Express.Js,MongoDB.</p>
+    <a href="https://github.com/MananKukreja24/MITgram" class="btn btn-primary">See Project</a>
+  </div>
+</div>
+
+            </div>
+        </div>
+
+    </div>
+    
+
+</section>
+</section>
+<section class="my-5">
+    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/proj3.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Log-Horizon</h4>
+    <p class="card-text">A project where i have used Next.js and worked under Gamedoora.</p>
+    <a href="https://github.com/MananKukreja24/log-horizon" class="btn btn-primary">See Project</a>
+  </div>
+</div>
+
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/proj2.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Injury-recognition</h4>
+    <p class="card-text">A project using python to recognise Injury.</p>
+    <a href="https://github.com/MananKukreja24/Injury_Recognition_Project" class="btn btn-primary">See Project</a>
+  </div>
+</div>
+
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/proj1.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Comming Soon</h4>
+    <p class="card-text">Some more projects under development.</p>
+    <a href="https://github.com/MananKukreja24/MITgram" class="btn btn-primary">See Project</a>
+  </div>
+</div>
+
+            </div>
+        </div>
+
+    </div>
+    
+
+</section>
+<section class="my-5">
+    <div class="py-5"> 
+        <h2 class="text-center">
+            Contact
+        </h2>
+    </div>
+    <div class="w-50 m-auto">
+      <form action="user-info.php" method="post">
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" name="user" autocomplete="off" class="form-control" placeholder="Manan Kukreja">
+          <label>Email</label>
+          <input type="text" name="email" autocomplete="off" class="form-control" placeholder="abc@gmail.com">
+          <label>Mobile no</label>
+          <input type="text" name="mobile" autocomplete="off" class="form-control" placeholder="9451XXXXXX">
+          <label>Social media link</label>
+          <input type="link" name="social" autocomplete="off" class="form-control" placeholder="Github.com/MananKukreja24">
+          <label>Comment</label>
+          <textarea class="form-control" name="comments" placeholder="Type your message here" ></textarea>
+          
+
+        </div>
+        <button type="submit" class="btn btn-success">Submit</button>
+
+      </form>
+    </div>
+</section>
+<footer>
+<p class="p-3 bg-dark text-white text-center">LinkedIn Id: <a href="https://www.linkedin.com/in/manan-kukreja-9a3a72231">Click Here!!</a></p>
+<p class="p-3 bg-dark text-white text-center">Github Id: <a href="https://github.com/MananKukreja24">Click Here!!</a></p>
+
+ 
+</footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
